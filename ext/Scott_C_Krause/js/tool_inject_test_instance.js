0024090f51c756cf2cef1ff82d2dd77e6b717d24
scott krause <cv@neostudios.com>
2016-03-01 07:59:04 -0600

    ux

--- ext/Scott_C_Krause/js/tool_inject_test_instance.js
+++ ext/Scott_C_Krause/js/tool_inject_test_instance.js
@@ -1,7 +1,3 @@
-var elemDiv = document.createElement('div');
-elemDiv.style.cssText = 'position:absolute;width:100%;height:100%;opacity:0.3;z-index:100;background:#000;';
-document.body.appendChild(elemDiv);
-
 var cookie_name ="", cookie_value="";
 chrome.storage.local.get("cookie_name" , function(fetchedData){
     cookie_name = fetchedData["cookie_name"];
@@ -11,12 +7,3 @@ chrome.storage.local.get("cookie_name" , function(fetchedData){
 		window.location.reload();
 	});
 });
-
-/*
-<a data-dropdown="drop1" aria-controls="drop1" aria-expanded="true" class="button expand ">Test Instance</a>
-<ul id="drop1" class="f-dropdown open f-open-dropdown" data-dropdown-content="" aria-hidden="false" tabindex="-1" style="position: absolute; left: 0px; top: 43px;">
-  <li><a href="#">This is a link</a></li>
-  <li><a href="#">This is another</a></li>
-  <li><a href="#">Yet another</a></li>
-</ul>
-*/
